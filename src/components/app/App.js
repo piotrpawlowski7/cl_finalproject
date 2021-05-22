32ecafa1a46504e2f54bc1fe7b6439730f39c416
piotrpawlowski7 <74098973+piotrpawlowski7@users.noreply.github.com>
2021-05-22 22:31:35 +0200

    Przygotowany layout

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,10 @@
 import logo from '../../assets/logo.svg';
 import './App.scss';
 
+import Navigation from "../navigation/Navigation";
 import Header from "../header/Header";
 import Main from "../main/Main";
+import Footer from "../footer/Footer";
 // import Button from 'react-bootstrap/Button';
 
 
@@ -10,9 +12,10 @@ import Main from "../main/Main";
 function App() {
   return (
     <>
+    <Navigation></Navigation>
     <Header></Header>
     <Main></Main>
-   
+   <Footer></Footer>
 
     </>
   //   <div className="App">
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,26 +1,12 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+
 import Tagscloud from '../Tagscloud/Tagscloud';
+import "./Header.scss"
 
 function Header() {
 return (
 <>
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
-    </Form>
-  </Navbar>
-
+  
+<header>
   <div className="container">
       <div className="row justify-content-center">
        <h1>h1. Boostrap heading</h1>
@@ -29,8 +15,10 @@ return (
        <p>Lead paragraph. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</p>
        </div>
       </div>
-
+      </header>
+      <section className="header__tagscloud">
       <Tagscloud></Tagscloud>
+      </section>
 </>
 );
 }
